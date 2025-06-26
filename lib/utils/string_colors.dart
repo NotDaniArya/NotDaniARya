076f3cc38dd236389c24fb27dce6355ce100beef
Muhammad Dani Arya Putra <13120210004@umi.ac.id>
2025-06-26 13:23:23 +0800

    feat : menambahkan string_colors.dart dan string_images_network.dart

---
+++ lib/utils/string_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class RColor {
+  static const Color primary = Color.fromARGB(255, 52, 56, 73);
+  static const Color onPrimary = Color.fromARGB(255, 36, 39, 52);
+  static const Color secondary = Color.fromARGB(255, 40, 42, 55);
+  static const Color widGrenn = Color.fromARGB(255, 31, 216, 111);
+}
---
+++ lib/utils/string_images_network.dart
@@ -0,0 +1,4 @@
+class RImages {
+  static const String project =
+      'https://sumeks.disway.id/upload/2bd7f9825dd27cffa4ed63eab0fa50ab.png';
+}
